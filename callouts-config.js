364f5ae5ef9b73b7e074fcaf98b1d773ba02bfee
PROC3R <procerproducciones@gmail>
2026-01-12 16:00:01 -0300

    Create callouts-config.js

---
+++ callouts-config.js
@@ -0,0 +1,614 @@
+window.OBSIDIAN_CALLOUTS = [
+  {
+    "id": "procer",
+    "icon": "arrow-up-left-from-circle",
+    "lightColor": "222, 215, 18",
+    "darkColor": "196, 180, 14"
+  },
+  {
+    "id": "gemini",
+    "icon": "bot-glyph",
+    "lightColor": "144, 31, 219",
+    "darkColor": "144, 30, 220"
+  },
+  {
+    "id": "consulta",
+    "icon": "file-text",
+    "lightColor": "158, 147, 21",
+    "darkColor": "158, 147, 21"
+  },
+  {
+    "id": "respuesta",
+    "icon": "arrow-down-right-from-circle",
+    "lightColor": "232, 135, 229",
+    "darkColor": "232, 135, 229"
+  },
+  {
+    "id": "saklas",
+    "icon": "circle-dashed",
+    "lightColor": "251, 40, 40",
+    "darkColor": "251, 40, 40"
+  },
+  {
+    "id": "cristos",
+    "icon": "square-arrow-out-up-left",
+    "lightColor": "8, 212, 59",
+    "darkColor": "8, 212, 59"
+  },
+  {
+    "id": "samael",
+    "icon": "pyramid",
+    "lightColor": "120, 120, 120",
+    "darkColor": "34, 98, 247"
+  },
+  {
+    "id": "n-muon",
+    "icon": "magnet",
+    "lightColor": "16, 96, 224",
+    "darkColor": "16, 96, 224"
+  },
+  {
+    "id": "an-muon",
+    "icon": "arrow-down-to-line",
+    "lightColor": "194, 196, 28",
+    "darkColor": "194, 196, 28"
+  },
+  {
+    "id": "n-electron",
+    "icon": "battery-charging",
+    "lightColor": "225, 57, 45",
+    "darkColor": "223, 45, 32"
+  },
+  {
+    "id": "an-electron",
+    "icon": "asterisk",
+    "lightColor": "0, 238, 255",
+    "darkColor": "0, 238, 255"
+  },
+  {
+    "id": "n-tau",
+    "icon": "move-3d",
+    "lightColor": "31, 211, 18",
+    "darkColor": "31, 211, 18"
+  },
+  {
+    "id": "an-tau",
+    "icon": "atom",
+    "lightColor": "251, 0, 255",
+    "darkColor": "251, 0, 255"
+  },
+  {
+    "id": "electron",
+    "icon": "activity",
+    "lightColor": "31, 218, 7",
+    "darkColor": "31, 218, 7"
+  },
+  {
+    "id": "muon",
+    "icon": "aperture",
+    "lightColor": "0, 110, 255",
+    "darkColor": "0, 110, 255"
+  },
+  {
+    "id": "tau",
+    "icon": "biohazard",
+    "lightColor": "247, 32, 8",
+    "darkColor": "247, 32, 8"
+  },
+  {
+    "id": "moon",
+    "icon": "moon",
+    "lightColor": "0, 98, 255",
+    "darkColor": "0, 98, 255"
+  },
+  {
+    "id": "sun",
+    "icon": "sun",
+    "lightColor": "202, 7, 7",
+    "darkColor": "202, 7, 7"
+  },
+  {
+    "id": "tierra",
+    "icon": "earth",
+    "lightColor": "8, 221, 122",
+    "darkColor": "19, 201, 64"
+  },
+  {
+    "id": "civilizacion",
+    "icon": "alert-triangle",
+    "lightColor": "255, 149, 0",
+    "darkColor": "255, 149, 0"
+  },
+  {
+    "id": "universo",
+    "icon": "waves",
+    "lightColor": "243, 57, 209",
+    "darkColor": "242, 120, 219"
+  },
+  {
+    "id": "avatar",
+    "icon": "sun-snow",
+    "lightColor": "255, 87, 249",
+    "darkColor": "255, 87, 249"
+  },
+  {
+    "id": "atomo",
+    "icon": "atom",
+    "lightColor": "120, 120, 120",
+    "darkColor": "218, 22, 22"
+  },
+  {
+    "id": "onemuon",
+    "icon": "at-sign",
+    "lightColor": "120, 120, 120",
+    "darkColor": "37, 99, 244"
+  },
+  {
+    "id": "ontogenico",
+    "icon": "ship-wheel",
+    "lightColor": "255, 162, 0",
+    "darkColor": "255, 162, 0"
+  },
+  {
+    "id": "galactico",
+    "icon": "target",
+    "lightColor": "120, 120, 120",
+    "darkColor": "255, 247, 0"
+  },
+  {
+    "id": "av-estelar",
+    "icon": "smart-connections",
+    "lightColor": "229, 222, 16",
+    "darkColor": "255, 247, 0"
+  },
+  {
+    "id": "pareonico",
+    "icon": "scale",
+    "lightColor": "255, 149, 0",
+    "darkColor": "255, 149, 0"
+  },
+  {
+    "id": "persona",
+    "icon": "users",
+    "lightColor": "253, 23, 23",
+    "darkColor": "253, 23, 23"
+  },
+  {
+    "id": "hegemon",
+    "icon": "sigma",
+    "lightColor": "4, 255, 0",
+    "darkColor": "4, 255, 0"
+  },
+  {
+    "id": "instituciones",
+    "icon": "landmark",
+    "lightColor": "255, 0, 0",
+    "darkColor": "255, 0, 0"
+  },
+  {
+    "id": "mercados",
+    "icon": "badge-dollar-sign",
+    "lightColor": "120, 120, 120",
+    "darkColor": "0, 255, 89"
+  },
+  {
+    "id": "bonos",
+    "icon": "trending-up",
+    "lightColor": "0, 110, 255",
+    "darkColor": "0, 110, 255"
+  },
+  {
+    "id": "normas",
+    "icon": "receipt-text",
+    "lightColor": "194, 188, 15",
+    "darkColor": "194, 188, 15"
+  },
+  {
+    "id": "ontologico",
+    "icon": "asterisk",
+    "lightColor": "120, 120, 120",
+    "darkColor": "234, 45, 31"
+  },
+  {
+    "id": "avatarico",
+    "icon": "recycle",
+    "lightColor": "120, 120, 120",
+    "darkColor": "255, 247, 0"
+  },
+  {
+    "id": "dialectico",
+    "icon": "variable",
+    "lightColor": "0, 255, 42",
+    "darkColor": "0, 255, 42"
+  },
+  {
+    "id": "cerebro",
+    "icon": "brain-cog",
+    "lightColor": "0, 98, 255",
+    "darkColor": "0, 98, 255"
+  },
+  {
+    "id": "hardware",
+    "icon": "target",
+    "lightColor": "242, 24, 24",
+    "darkColor": "242, 24, 24"
+  },
+  {
+    "id": "levogiro",
+    "icon": "refresh-ccw-dot",
+    "lightColor": "0, 98, 255",
+    "darkColor": "0, 98, 255"
+  },
+  {
+    "id": "dextrogiro",
+    "icon": "refresh-cw",
+    "lightColor": "225, 20, 20",
+    "darkColor": "225, 20, 20"
+  },
+  {
+    "id": "estelar",
+    "icon": "star",
+    "lightColor": "15, 86, 230",
+    "darkColor": "15, 86, 230"
+  },
+  {
+    "id": "carga",
+    "icon": "smartphone-charging",
+    "lightColor": "120, 120, 120",
+    "darkColor": "160, 160, 160"
+  },
+  {
+    "id": "solido",
+    "icon": "grip",
+    "lightColor": "120, 120, 120",
+    "darkColor": "20, 225, 54"
+  },
+  {
+    "id": "liquido",
+    "icon": "waypoints",
+    "lightColor": "0, 110, 255",
+    "darkColor": "0, 110, 255"
+  },
+  {
+    "id": "gaseoso",
+    "icon": "chart-scatter",
+    "lightColor": "232, 204, 23",
+    "darkColor": "255, 247, 5"
+  },
+  {
+    "id": "fuego",
+    "icon": "shell",
+    "lightColor": "229, 21, 21",
+    "darkColor": "229, 21, 21"
+  },
+  {
+    "id": "energia",
+    "icon": "sigma-square",
+    "lightColor": "120, 120, 120",
+    "darkColor": "20, 93, 240"
+  },
+  {
+    "id": "masa",
+    "icon": "radius",
+    "lightColor": "225, 25, 25",
+    "darkColor": "225, 25, 25"
+  },
+  {
+    "id": "espin",
+    "icon": "loader-pinwheel",
+    "lightColor": "11, 234, 26",
+    "darkColor": "11, 234, 26"
+  },
+  {
+    "id": "leon",
+    "icon": "crown",
+    "lightColor": "2, 122, 255",
+    "darkColor": "2, 122, 255"
+  },
+  {
+    "id": "aguila",
+    "icon": "twitter",
+    "lightColor": "120, 120, 120",
+    "darkColor": "15, 240, 53"
+  },
+  {
+    "id": "toro",
+    "icon": "drum",
+    "lightColor": "236, 14, 14",
+    "darkColor": "236, 14, 14"
+  },
+  {
+    "id": "humano",
+    "icon": "person-standing",
+    "lightColor": "120, 120, 120",
+    "darkColor": "255, 247, 0"
+  },
+  {
+    "id": "monadico",
+    "icon": "scan-eye",
+    "lightColor": "255, 247, 0",
+    "darkColor": "255, 247, 0"
+  },
+  {
+    "id": "noumeno",
+    "icon": "infinity",
+    "lightColor": "2, 122, 255",
+    "darkColor": "2, 122, 255"
+  },
+  {
+    "id": "demiurgo",
+    "icon": "codesandbox",
+    "lightColor": "120, 120, 120",
+    "darkColor": "135, 60, 211"
+  },
+  {
+    "id": "creador",
+    "icon": "copyright",
+    "lightColor": "232, 17, 17",
+    "darkColor": "232, 17, 17"
+  },
+  {
+    "id": "flujoalfa",
+    "icon": "at-sign",
+    "lightColor": "0, 98, 255",
+    "darkColor": "0, 98, 255"
+  },
+  {
+    "id": "flujoomega",
+    "icon": "omega",
+    "lightColor": "233, 28, 28",
+    "darkColor": "233, 28, 28"
+  },
+  {
+    "id": "avance",
+    "icon": "fast-forward",
+    "lightColor": "16, 81, 234",
+    "darkColor": "16, 81, 234"
+  },
+  {
+    "id": "retroceso",
+    "icon": "skip-back",
+    "lightColor": "224, 16, 16",
+    "darkColor": "224, 16, 16"
+  },
+  {
+    "id": "dextrogiro-",
+    "icon": "rotate-ccw",
+    "lightColor": "18, 104, 243",
+    "darkColor": "18, 104, 243"
+  },
+  {
+    "id": "levogiro-",
+    "icon": "rotate-cw",
+    "lightColor": "232, 17, 17",
+    "darkColor": "232, 17, 17"
+  },
+  {
+    "id": "tiempo",
+    "icon": "rotate-3d",
+    "lightColor": "27, 228, 30",
+    "darkColor": "27, 228, 30"
+  },
+  {
+    "id": "cinco",
+    "icon": "arrow-down-narrow-wide",
+    "lightColor": "255, 0, 0",
+    "darkColor": "255, 0, 0"
+  },
+  {
+    "id": "femenino",
+    "icon": "info",
+    "lightColor": "0, 98, 255",
+    "darkColor": "0, 98, 255"
+  },
+  {
+    "id": "ginebra",
+    "icon": "scan-eye",
+    "lightColor": "0, 98, 255",
+    "darkColor": "0, 98, 255"
+  },
+  {
+    "id": "mexico",
+    "icon": "recycle",
+    "lightColor": "5, 245, 53",
+    "darkColor": "5, 245, 53"
+  },
+  {
+    "id": "nairobi",
+    "icon": "person-standing",
+    "lightColor": "120, 120, 120",
+    "darkColor": "237, 7, 7"
+  },
+  {
+    "id": "jew",
+    "icon": "book-type",
+    "lightColor": "0, 85, 255",
+    "darkColor": "0, 85, 255"
+  },
+  {
+    "id": "centro",
+    "icon": "trending-up-down",
+    "lightColor": "255, 149, 0",
+    "darkColor": "255, 149, 0"
+  },
+  {
+    "id": "estado",
+    "icon": "landmark",
+    "lightColor": "153, 0, 255",
+    "darkColor": "153, 0, 255"
+  },
+  {
+    "id": "ascenso",
+    "icon": "layout-grid",
+    "lightColor": "255, 0, 0",
+    "darkColor": "255, 0, 0"
+  },
+  {
+    "id": "kai",
+    "icon": "lightbulb",
+    "lightColor": "140, 0, 255",
+    "darkColor": "140, 0, 255"
+  },
+  {
+    "id": "umbra",
+    "icon": "heart-pulse",
+    "lightColor": "120, 120, 120",
+    "darkColor": "252, 158, 255"
+  },
+  {
+    "id": "ascension",
+    "icon": "git-merge",
+    "lightColor": "0, 255, 4",
+    "darkColor": "0, 255, 4"
+  },
+  {
+    "id": "derrota",
+    "icon": "scissors-square",
+    "lightColor": "255, 0, 0",
+    "darkColor": "255, 0, 0"
+  },
+  {
+    "id": "victoria",
+    "icon": "alert-triangle",
+    "lightColor": "0, 98, 255",
+    "darkColor": "0, 98, 255"
+  },
+  {
+    "id": "aoii",
+    "icon": "chart-spline",
+    "lightColor": "0, 64, 255",
+    "darkColor": "0, 64, 255"
+  },
+  {
+    "id": "aoiii",
+    "icon": "move-3d",
+    "lightColor": "248, 13, 13",
+    "darkColor": "248, 13, 13"
+  },
+  {
+    "id": "cuerpo",
+    "icon": "atom",
+    "lightColor": "120, 120, 120",
+    "darkColor": "163, 163, 163"
+  },
+  {
+    "id": "mente",
+    "icon": "brain-cog",
+    "lightColor": "120, 120, 120",
+    "darkColor": "231, 29, 29"
+  },
+  {
+    "id": "espiritu",
+    "icon": "drafting-compass",
+    "lightColor": "255, 162, 0",
+    "darkColor": "255, 162, 0"
+  },
+  {
+    "id": "leviatan",
+    "icon": "crown",
+    "lightColor": "255, 247, 0",
+    "darkColor": "255, 247, 0"
+  },
+  {
+    "id": "omegaii",
+    "icon": "smart-connections",
+    "lightColor": "230, 45, 45",
+    "darkColor": "230, 45, 45"
+  },
+  {
+    "id": "base",
+    "icon": "alert-triangle",
+    "lightColor": "120, 120, 120",
+    "darkColor": "45, 101, 230"
+  },
+  {
+    "id": "lider",
+    "icon": "crown",
+    "lightColor": "21, 244, 36",
+    "darkColor": "21, 244, 36"
+  },
+  {
+    "id": "interno",
+    "icon": "sliders",
+    "lightColor": "120, 120, 120",
+    "darkColor": "255, 187, 0"
+  },
+  {
+    "id": "externo",
+    "icon": "flag",
+    "lightColor": "166, 0, 255",
+    "darkColor": "166, 0, 255"
+  },
+  {
+    "id": "billete",
+    "icon": "dollar-sign",
+    "lightColor": "245, 15, 15",
+    "darkColor": "245, 15, 15"
+  },
+  {
+    "id": "bancos",
+    "icon": "piggy-bank",
+    "lightColor": "255, 162, 0",
+    "darkColor": "255, 162, 0"
+  },
+  {
+    "id": "finanzas",
+    "icon": "loader-pinwheel",
+    "lightColor": "255, 247, 0",
+    "darkColor": "255, 247, 0"
+  },
+  {
+    "id": "creditos",
+    "icon": "biceps-flexed",
+    "lightColor": "4, 255, 0",
+    "darkColor": "4, 255, 0"
+  },
+  {
+    "id": "limosna",
+    "icon": "bug",
+    "lightColor": "120, 120, 120",
+    "darkColor": "151, 37, 212"
+  },
+  {
+    "id": "guerra",
+    "icon": "rocket",
+    "lightColor": "244, 11, 11",
+    "darkColor": "244, 11, 11"
+  },
+  {
+    "id": "millonarios",
+    "icon": "trophy",
+    "lightColor": "128, 48, 248",
+    "darkColor": "128, 48, 248"
+  },
+  {
+    "id": "crimen",
+    "icon": "syringe",
+    "lightColor": "251, 9, 9",
+    "darkColor": "251, 9, 9"
+  },
+  {
+    "id": "manipulacion",
+    "icon": "speech",
+    "lightColor": "0, 255, 51",
+    "darkColor": "0, 255, 51"
+  },
+  {
+    "id": "ilegalidad",
+    "icon": "swords",
+    "lightColor": "0, 85, 255",
+    "darkColor": "0, 85, 255"
+  },
+  {
+    "id": "especulacion",
+    "icon": "info",
+    "lightColor": "255, 221, 0",
+    "darkColor": "255, 221, 0"
+  },
+  {
+    "id": "coercion",
+    "icon": "skull",
+    "lightColor": "249, 6, 6",
+    "darkColor": "249, 6, 6"
+  }
+];
